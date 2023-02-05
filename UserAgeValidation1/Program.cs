5e31c5ee3147a6d8988c47c11c7bdfb3d9f59f0e
Liisa <liisa.pent@tktk.ee>
2023-02-05 14:32:48 +0200

    valmis

---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// rakendus küsib kasutaja vanust
+// kui kasutaja vanus on <13, kuvatakse konsoolis:
+// "You are too young to use Instagram"
+// muul juhul kuvatakse konsoolis: 
+// "Welcome to Instagram"
+
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine()); // "13" - heap, 13 - stack
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else // if (userAge < 13)
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
